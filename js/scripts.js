7625803f864cbf9cd5e7424e5f28ca19a1e8252b
Jared Reando and Tavish OKeefe <tavish@>
2018-10-29 16:35:09 -0700

    Add lots of problems. Solve nothing. Life is pain.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,105 @@
-function AddressBook() {
-  this.contacts = [],
-  this.currentId = 0
+// function AddressBook() {
+//   this.contacts = [],
+//   this.currentId = 0
+// }
+//
+// AddressBook.prototype.addContact = function(contact) {
+//   contact.id = this.assignID();
+//   this.contacts.push(contact);
+// }
+//
+// AddressBook.prototype.assignID = function(){
+//   this.currentId += 1;
+//   return this.currentId;
+// }
+//
+// AddressBook.prototype.findContact = function(id) {
+//   for (var i=0; i < this.contacts.length; i++) {
+//     if (this.contacts[i]) {
+//       if (this.contacts[i].id === id) {
+//         return this.contacts[i];
+//       }
+//     }
+//   };
+//     return false;
+//   }
+//
+// AddressBook.prototype.deleteContact = function(id) {
+//   for (var i=0; i < this.contacts.length; i++) {
+//     if (this.contacts[i]) {
+//       if (this.contacts[i].id === id) {
+//         delete this.contacts[i];
+//         return true;
+//       }
+//     }
+//   };
+//     return false;
+//   }
+//
+//
+// function Contact (firstName, lastName, phoneNumber) {
+//     this.firstName = firstName,
+//     this.lastName = lastName,
+//     this.phoneNumber = phoneNumber
+// };
+//
+//
+// Contact.prototype.fullName = function () {
+//   return this.firstName + " " + this.lastName;
+// };
+
+
+function Trips() {
+  this.places = [];
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignID();
-  this.contacts.push(contact);
+Trips.prototype.addTrip = function (trip) {
+  this.places.push(trip);
 }
 
-AddressBook.prototype.assignID = function(){
-  this.currentId += 1;
-  return this.currentId;
+function Place(location, terrain, timeVisited, notes) {
+  this.location = location,
+  this.terrain = terrain,
+  this.timeVisited = timeVisited,
+  this.notes = notes
 }
 
-AddressBook.prototype.findContact = function(id) {
-  for (var i=0; i < this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id === id) {
-        return this.contacts[i];
-      }
-    }
-  };
-    return false;
-  }
+// Cient end code
 
-AddressBook.prototype.deleteContact = function(id) {
-  for (var i=0; i < this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id === id) {
-        delete this.contacts[i];
-        return true;
-      }
-    }
-  };
-    return false;
-  }
+var myTrips = new Trips();
 
+$(function() {
+  $("p#clickHere").click(function() {
+    console.log("You've clicked the paragraph");
+  });
 
-function Contact (firstName, lastName, phoneNumber) {
-    this.firstName = firstName,
-    this.lastName = lastName,
-    this.phoneNumber = phoneNumber
-};
+  $("p#testy").click(function() {
+    console.log("Testy talks back!!");
+  });
 
+  $("#survey").submit(function(event) {
+    event.preventDefault();
 
-Contact.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName;
-};
+var location = $("input#location").val();
+var terrain = $("input#terrain").val();
+var dateVisited = $("input#dateVisited").val();
+var tripNotes = $("input#tripNotes").val();
 
-var john = new Contact ('j', 'k', '5');
+var add_location_log = new Place(location, terrain, dateVisited, tripNotes);
 
-var jim = new Contact ('jim', 'smith', '43');
+myTrips.addTrip(location);
 
-var joe = new Contact ('s', 'e', '2');
+var userResults = add_location_log;
 
-var newBook = new AddressBook();
+$("#results").text(userResults);
 
 
-newBook.addContact(john);
-newBook.addContact(jim);
-newBook.addContact(joe);
+
+
+console.log(location + terrain + dateVisited + tripNotes);
+
+    // var userInput = $(form#survey).val();
+    // var results = AddressBook(userInput);
+
+});
+});
